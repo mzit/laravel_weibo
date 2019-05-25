5e50de00cf38d54299e6263b26e9c1cb5e300e5e
mzit <admin@mzit.tech>
2019-05-25 10:36:08 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,13 +11,20 @@ class UsersController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth',[
-            'except' => ['show', 'create', 'store']
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store', 'index']
         ]);
         $this->middleware('guest', [
             'only' => ['create']
         ]);
     }
+
+    public function index()
+    {
+        $users = User::query()->paginate(10);
+        return view('users.index', compact('users'));
+    }
+
     public function create()
     {
         return view('users.create');
@@ -30,7 +37,7 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate($request,[
+        $this->validate($request, [
             'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
@@ -48,23 +55,23 @@ class UsersController extends Controller
     public function edit(User $user)
     {
         $this->authorize('update', $user);
-        return view('users.edit',compact('user'));
+        return view('users.edit', compact('user'));
     }
 
-    public function update(User $user,Request $request)
+    public function update(User $user, Request $request)
     {
         $this->authorize('update', $user);
-        $this->validate($request,[
+        $this->validate($request, [
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
         ]);
         $update = [];
         $update['name'] = $request->name;
-        if ($request->password){
+        if ($request->password) {
             $update['password'] = bcrypt($request->password);
         }
         $user->update($update);
         session()->flash('success', '个人资料更新成功！');
-        return redirect()->route('users.show',$user->id);
+        return redirect()->route('users.show', $user->id);
     }
 }
